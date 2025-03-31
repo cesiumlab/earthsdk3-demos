c72905adccc94c73a7ff891e75680c5027f17336
杨树彭 <suplyang@163.com>
2025-03-31 11:29:15 +0800

    更新 README，调整项目配置，修复 earthsdk3-assets 路径，修改 package.json 隐私设置

--- README.md
+++ README.md
@@ -8,6 +8,6 @@
 - earthsdk3-openlayers 是earthsdk一个自定义引擎扩展和自定义对象扩展的示例，使用openlayers作为渲染引擎。
 
 # 使用
- - 推荐使用 pnpm ( 安装命令: npm install -g pnpm )
+ - 推荐使用 pnpm 和 vite 搭建的项目来集成 earthsdk ( 安装命令: npm install -g pnpm )
  - 任意目录运行 pnpm install 安装依赖
  - 对应目录下运行 pnpm dev/xxx
--- examples/earthsdk3-wbpk/vue.config.js
+++ examples/earthsdk3-wbpk/vue.config.js
@@ -5,8 +5,14 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const cesiumSource = 'node_modules/cesium/Source';
 const cesiumWorkers = "../Build/Cesium/Workers";
 const cesiumBaseUrl = "cesiumStatic";
-
+// cesium官方配置: https://cesium.com/blog/2024/02/13/configuring-vite-or-webpack-for-cesiumjs/
 module.exports = defineConfig({
+  devServer: {
+    client: {
+      progress: true,
+      overlay: false,
+    },
+  },
   transpileDependencies: true,
   configureWebpack: {
     resolve: {
@@ -22,7 +28,7 @@ module.exports = defineConfig({
           { from: path.join(cesiumSource, "ThirdParty"), to: `${cesiumBaseUrl}/ThirdParty`, },
           { from: path.join(cesiumSource, "Assets"), to: `${cesiumBaseUrl}/Assets`, },
           { from: path.join(cesiumSource, "Widgets"), to: `${cesiumBaseUrl}/Widgets`, },
-          { from: path.join('node_modules/earthsdk3-assets'), to: 'js/earthsdk3-assets' },
+          { from: path.join('../../node_modules/earthsdk3-assets'), to: 'js/earthsdk3-assets' },
         ],
       }),
       new webpack.DefinePlugin({

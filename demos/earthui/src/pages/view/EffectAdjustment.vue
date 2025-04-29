68dca0977cb195ffbb296ce5689cae6dbb2504ec
qinying <2832938372@qq.com>
2025-04-29 15:32:16 +0800

    添加编辑高度偏移功能

--- demos/earthui/src/pages/view/EffectAdjustment.vue
+++ demos/earthui/src/pages/view/EffectAdjustment.vue
@@ -12,15 +12,20 @@ const textAvoidance = toVR<boolean>(d, [xbsjEarthUi.activeViewer, 'textAvoidance
 const objType = ref('')
 </script>
 <template>
-    <RightList :title="'全局效果调整'">
+     <RightList :title="'全局效果调整'">
         <Button :name="'wenzibirang'" :content="'文字避让'" :actived="textAvoidance === true"
             :click="() => { textAvoidance = !textAvoidance }" :left-button="true"></Button>
         <Button :name="'FOV1'" :content="'FOV'" :actived="objType === 'fov'"
             :click="() => { objType === 'fov' ? objType = '' : objType = 'fov' }" :left-button="false"></Button>
         <Button :name="'dingweipeizhi'" :content="'定位配置'" :actived="objType === 'flyToBoundingSize'"
-            :click="() => { objType === 'flyToBoundingSize' ? objType = '' : objType = 'flyToBoundingSize' }" :left-button="true"></Button>
+            :click="() => { objType === 'flyToBoundingSize' ? objType = '' : objType = 'flyToBoundingSize' }"
+            :left-button="true"></Button>
+        <Button :name="'pianyi'" :content="'编辑高度偏移'" :actived="objType === 'editingHeightOffset'"
+            :click="() => { objType === 'editingHeightOffset' ? objType = '' : objType = 'editingHeightOffset' }"
+            :left-button="false"></Button>
         <Fov v-if="objType === 'fov'"></Fov>
         <FlyToBoundingSize v-if="objType === 'flyToBoundingSize'"></FlyToBoundingSize>
+        <EditingHeightOffset v-if="objType === 'editingHeightOffset'"></EditingHeightOffset>
     </RightList>
 </template>
 <style scoped></style>

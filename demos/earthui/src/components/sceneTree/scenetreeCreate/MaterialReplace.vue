fcb074af48fbed647f0bb928c0f3be6e536bf1fe
张名师 <1368953782@qq.com>
2025-06-05 16:22:54 +0800

    更新材质替换器布局

--- demos/earthui/src/components/sceneTree/scenetreeCreate/MaterialReplace.vue
+++ demos/earthui/src/components/sceneTree/scenetreeCreate/MaterialReplace.vue
@@ -3,12 +3,6 @@
         :title="`材质替换编辑器${(props.setStyleTreeItem && props.setStyleTreeItem.name) ? '（' + props.setStyleTreeItem.name + '）' : ''}`"
         :width="600" :height="'440px'" :left="650" :top="200" @close="changeCancel" :showButton="true" @ok="changeOk">
         <div class="material">
-            <div class="header">
-                <input type="text" v-model="materialName" placeholder="请输入材质名称">
-                <div @click="searchMaterial">
-                    <img class="search_img" src="../../../assets/material/search.png" alt="">
-                </div>
-            </div>
             <div class="middle">
                 <div class="middle_title">
                     <p>Tiles材质名称</p>
@@ -18,9 +12,8 @@
                     <div v-for="(item, index) in list" :key="index">
                         <input type="checkbox" v-model="item.select">
                         <p>{{ item.key }}</p>
-                        <p>{{ item.value }}</p>
+                        <input type="text" v-model="item.value">
                         <img src="../../../assets/material/caizhi_weixuanzhong.png" alt="" @click="setMaterial(item)">
-                        <img src="../../../assets/material/chongzhi_weixuanzhong.png" alt="">
 
                     </div>
                 </div>
@@ -128,12 +121,6 @@ const okMaterialSelect = async (material: string) => {
 
 
 /**
- * 搜索材质
- */
-const searchMaterial = () => {
-
-}
-/**
  * 获取材质名称列表
  */
 const getMaterialNameList = async () => {
@@ -198,52 +185,14 @@ onMounted(() => {
 <style scoped>
 .material {
     width: 100%;
-    height: 100%;
+    height: calc(100% - 60px);
 
 }
 
-.header {
-    width: calc(100% - 30px);
-    height: 32px;
-    margin: auto;
-    display: flex;
-    align-items: center;
-    padding-top: 20px;
-    color: #fff;
-}
-
-.header input {
-    width: calc(100% - 40px);
-    height: 100%;
-    background: #25262A;
-    border-radius: 4px 0px 0px 4px;
-    border: 1px solid #5d5e60;
-    text-indent: 15px;
-    box-sizing: border-box;
-    color: #fff;
-}
-
-.header div {
-    width: 40px;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: 1px solid #5d5e60;
-    box-sizing: border-box;
-    border-radius: 0px 4px 4px 0px;
-    border-left: 0px;
-    cursor: pointer;
-}
-
-.search_img:active,
-.search_img:hover {
-    content: url(../../../assets/material/search_hover.png);
-}
 
 .middle {
     width: calc(100% - 30px);
-    height: calc(100% - 130px);
+    height: 100%;
     margin: auto;
 }
 
@@ -272,7 +221,8 @@ onMounted(() => {
 
 .middle_content div {
     display: grid;
-    grid-template-columns: 40px 1fr 1fr 20px 20px;
+    grid-template-columns: 40px 1fr 1fr 30px;
+    grid-column-gap: 15px;
     align-items: center;
     text-align: center;
     font-size: 12px;
@@ -280,11 +230,24 @@ onMounted(() => {
 
 .middle_content div p {
     height: 20px;
+    margin: 4px;
+    margin-right: 20px;
+}
+
+.middle_content div input {
     background: rgba(28, 28, 29, 0.6);
     border-radius: 4px;
     border: 1px solid #3B3C40;
-    margin: 4px;
-    margin-right: 20px;
+    color: #fff;
+    text-align: center;
+}
+
+.middle_content div img {
+    cursor: pointer;
+}
+
+.middle_content div img:hover {
+    content: url(../../../assets/material/caizhi_xuanzhong.png);
 }
 
 .footer {

aafd743706075dee62d09c21dd781fb3ffc6a4fa
张名师 <1368953782@qq.com>
2025-06-18 10:50:57 +0800

    更新示例代码js更新为iife.js

--- demos/earthsdk3-usage/vite.config.ts
+++ demos/earthsdk3-usage/vite.config.ts
@@ -3,36 +3,36 @@ import path from 'path';
 import { defineConfig, normalizePath } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 export default defineConfig({
-  base: './',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  plugins: [
-    vue(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: [
-            normalizePath(path.resolve(__dirname, './node_modules/xbsj-base/dist/xbsj-base.js')),
-            normalizePath(path.resolve(__dirname, './node_modules/earthsdk3-assets')),
-            normalizePath(path.resolve(__dirname, './node_modules/earthsdk3/dist/earthsdk3.js')),
-            normalizePath(path.resolve(__dirname, './node_modules/earthsdk3-cesium/dist/earthsdk3-cesium.js')),
-            normalizePath(path.resolve(__dirname, './node_modules/earthsdk3-ue/dist/earthsdk3-ue.js')),
-          ],
-          dest: './earthsdk3'
+    base: './',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src')
         }
-      ]
-    })
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-        example: path.resolve(__dirname, 'example.html'),
-      },
+    },
+    plugins: [
+        vue(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: [
+                        normalizePath(path.resolve(__dirname, './node_modules/xbsj-base/dist/xbsj-base.js')),
+                        normalizePath(path.resolve(__dirname, './node_modules/earthsdk3-assets')),
+                        normalizePath(path.resolve(__dirname, './node_modules/earthsdk3/dist/earthsdk3.iife.js')),
+                        normalizePath(path.resolve(__dirname, './node_modules/earthsdk3-cesium/dist/earthsdk3-cesium.iife.js')),
+                        normalizePath(path.resolve(__dirname, './node_modules/earthsdk3-ue/dist/earthsdk3-ue.iife.js')),
+                    ],
+                    dest: './earthsdk3'
+                }
+            ]
+        })
+    ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, 'index.html'),
+                example: path.resolve(__dirname, 'example.html'),
+            },
+        }
     }
-  }
 })
 

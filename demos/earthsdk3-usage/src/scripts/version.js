d69d615d7e2e0659efd532f91437ac45b1bda551
张名师 <1368953782@qq.com>
2025-06-27 11:20:56 +0800

    更新

--- demos/earthsdk3-usage/src/scripts/version.js
+++ demos/earthsdk3-usage/src/scripts/version.js
@@ -3,8 +3,8 @@ export const version = {
     "code": "https://gitee.com/cesiumlab/earthsdk3-code",
     "doc": "https://c0yh9tnn0na.feishu.cn/wiki/BI1Jwn6DsiOtlIk8OCLckNm5n92",
     //TODO:秦瑛 Update the version numbers below when a new version is released
-    "earthsdk3": "3.0.7",
-    "earthsdk3-cesium": "3.0.7",
-    "earthsdk3-ue": "3.0.7",
+    "earthsdk3": "3.2.0-beta.2",
+    "earthsdk3-cesium": "3.2.0-beta.2",
+    "earthsdk3-ue": "3.1.0",
     "earthsdk3-assets": "3.0.4",
 }

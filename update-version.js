b3045079ae5ca9985fce0a8d486a7372073f606a
xingenjun <1533572528@qq.com>
2025-07-08 11:46:58 +0800

    chore: 更新 earthsdk3 及相关依赖至最新 beta 版本
    将 earthsdk3 从 3.2.0-beta.4 升级至 3.2.0-beta.5
    将 earthsdk3-cesium 从 3.2.0-beta.6 升级至 3.2.0-beta.7
    将 earthsdk3-ue 从 3.2.0-beta.5 升级至 3.2.0-beta.7

--- update-version.js
+++ update-version.js
@@ -4,9 +4,9 @@ const path = require('path');
 // TODO:在此替换版本号 在根目录运行 node update-version.js (pnpm run update:version)
 const newVersion = {
     "xbsj-base": "1.0.3",
-    "earthsdk3": "3.2.0-beta.4",
-    "earthsdk3-cesium": "3.2.0-beta.6",
-    "earthsdk3-ue": "3.2.0-beta.5",
+    "earthsdk3": "3.2.0-beta.5",
+    "earthsdk3-cesium": "3.2.0-beta.7",
+    "earthsdk3-ue": "3.2.0-beta.7",
     "earthsdk3-assets": "3.0.4",
 }
 

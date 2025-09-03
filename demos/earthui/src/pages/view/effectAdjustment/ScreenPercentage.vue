d795b9487ff233baf0cbeeba5cd4643b141b569b
qinying <2832938372@qq.com>
2025-09-03 14:49:45 +0800

    优化画面比例组件UI

--- demos/earthui/src/pages/view/effectAdjustment/ScreenPercentage.vue
+++ demos/earthui/src/pages/view/effectAdjustment/ScreenPercentage.vue
@@ -1,16 +1,14 @@
 <template>
     <PopList :title="'画面比例'">
-        <LabelInputSliderReset v-model="screenPercentage" :label="'画面比例'" :step="1" :min="50" :max="250"
-            :defaultValue="50" :width="154">
-        </LabelInputSliderReset>
+        <LabelInput v-model="screenPercentage" :inputType="'number'" :label="'画面比例'" :unit="'%'"></LabelInput>
     </PopList>
 </template>
 <script setup lang="ts">
 import { createVueDisposer, toVR } from 'earthsdk-ui';
-import LabelInputSliderReset from "../../../components/LabelInputSliderReset.vue";
 import PopList from "../../../components/PopList.vue";
 import { onBeforeUnmount, inject } from "vue";
 import { XbsjEarthUi } from "../../../scripts/xbsjEarthUi";
+import LabelInput from "../../../components/LabelInput.vue"
 
 const d = createVueDisposer(onBeforeUnmount);
 const xbsjEarthUi = inject('xbsjEarthUi') as XbsjEarthUi

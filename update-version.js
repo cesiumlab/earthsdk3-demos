ee631aa30a023d2ba2cb9dfdacaf1cd2e27f6c17
xingenjun <1533572528@qq.com>
2025-09-09 08:49:29 +0800

    更新所有包的版本号

--- update-version.js
+++ update-version.js
@@ -4,9 +4,9 @@ const path = require('path');
 // TODO:在此替换版本号 在根目录运行 node update-version.js (pnpm run update:version)
 const newVersion = {
     "xbsj-base": "1.0.6",
-    "earthsdk3": "3.4.0-beta.17",
-    "earthsdk3-cesium": "3.4.0-beta.9",
-    "earthsdk3-ue": "3.4.0-beta.8",
+    "earthsdk3": "3.4.0-beta.18",
+    "earthsdk3-cesium": "3.4.0-beta.10",
+    "earthsdk3-ue": "3.4.0-beta.9",
     "earthsdk3-assets": "3.0.6",
 }
 

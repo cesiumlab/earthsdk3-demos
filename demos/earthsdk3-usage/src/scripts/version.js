c77d86a68c0ecc099c1da3efa5e85495e06fa7c3
zhangmingshi <1368953782@qq.com>
2025-09-26 14:53:16 +0800

    【更新】更新API文档

--- demos/earthsdk3-usage/src/scripts/version.js
+++ demos/earthsdk3-usage/src/scripts/version.js
@@ -1,5 +1,5 @@
 export const version = {
-    "api": "https://c0yh9tnn0na.feishu.cn/wiki/NcIww74yTiNq0EkVxszc5XtOnqf",
+    "api": "https://www.earthsdk.com/docs/index.html",
     "code": "https://gitee.com/cesiumlab/earthsdk3-code",
     "doc": "https://c0yh9tnn0na.feishu.cn/wiki/BI1Jwn6DsiOtlIk8OCLckNm5n92",
     //TODO:秦瑛 Update the version numbers below when a new version is released

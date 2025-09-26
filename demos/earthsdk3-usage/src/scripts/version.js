56c008e99439b95cf0c8bbe47a506c90577ab342
zhangmingshi <1368953782@qq.com>
2025-09-26 14:24:10 +0800

    【更新】更新版本信息

--- demos/earthsdk3-usage/src/scripts/version.js
+++ demos/earthsdk3-usage/src/scripts/version.js
@@ -3,9 +3,9 @@ export const version = {
     "code": "https://gitee.com/cesiumlab/earthsdk3-code",
     "doc": "https://c0yh9tnn0na.feishu.cn/wiki/BI1Jwn6DsiOtlIk8OCLckNm5n92",
     //TODO:秦瑛 Update the version numbers below when a new version is released
-    "earthsdk3": "3.2.0-beta.2",
-    "earthsdk3-cesium": "3.2.0-beta.2",
-    "earthsdk3-ue": "3.1.0",
-    "earthsdk3-assets": "3.0.4",
-    "cesium": "1.132",
+    "cesium": "1.133.1",
+    "earthsdk3": "3.4.2",
+    "earthsdk3-cesium": "3.4.6",
+    "earthsdk3-ue": "3.4.2",
+    "earthsdk3-assets": "3.0.6",
 }
